74552b1e7d6ec40c44f9dcec5a7c40d9ba90abec
Deluce <ziyun.en@outlook.com>
2019-02-24 15:19:22 -0500

    new imgs

--- assets/js/filter-images.js
+++ assets/js/filter-images.js
@@ -1,16 +1,24 @@
 /*attribute with specific values*/
 $(document).ready(function() {
-  $('#awbtn').on('click', function() {
-    $("div[id=practices]").fadeIn('slow');
-    $("div[id=finished]").fadeIn('slow');
+  $('#all_btn').on('click', function() {
+    $("div[id=card-design]").fadeIn('slow');
+    $("div[id=game-jam]").fadeIn('slow');
+    $("div[id=others]").fadeIn('slow');
 });
-  $('#finibtn').on('click', function() {
-    $("div[id=practices]").fadeOut('slow');
-    $("div[id=finished]").fadeIn('slow');
-});
-  $('#ptcbtn').on('click', function() {
-    $("div[id=finished]").fadeOut('slow');
-    $("div[id=practices]").fadeIn('slow');
+  $('#cd_btn').on('click', function() {
+    $("div[id=game-jam]").fadeOut('slow');
+    $("div[id=others]").fadeOut('slow');
+    $("div[id=card-design]").fadeIn('slow');
 
+});
+  $('#gj_btn').on('click', function() {
+    $("div[id=card-design]").fadeOut('slow');
+    $("div[id=others]").fadeOut('slow');
+    $("div[id=game-jam]").fadeIn('slow');
+});
+  $('#otr_btn').on('click', function() {
+    $("div[id=card-design]").fadeOut('slow');
+    $("div[id=game-jam]").fadeOut('slow');
+    $("div[id=others]").fadeIn('slow');
 });
 });
